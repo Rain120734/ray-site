f7abc2f45d6df63d23895ba805f7d5edade5d700
stevehelloworld <steve0525@gmail.com>
2026-06-11 02:16:39 +0800

    fix: load photos from Google Drive direct links and untrack local images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["192.168.1.113:3000", "http://localhost:3000"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "docs.google.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
